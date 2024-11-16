2ff323118e79cc85053684bc6ac2d2584ffda366
nikilodiym <nikitabaram@gmail.com>
2024-11-16 16:51:22 +0200

    Add C# program to count vowels in a user-input string
    Introduced a new C# program in `task8.cs` with necessary `using` directives. Created a namespace `strings_and_arrays` and defined an internal class `task8`. Added a `Main` method to prompt user input and call `CountVowels` method. Implemented `CountVowels` to count and return the number of vowels in the input string.

---
+++ task8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_and_arrays
+{
+    internal class task8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+
+            int vowelCount = CountVowels(str);
+            Console.WriteLine("Number of vowels: " + vowelCount);
+        }
+
+        static int CountVowels(string str)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+
+            foreach (char c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
