6d271ae800e25fdc359c475770d5db37cbc81d93
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 14:37:34 +0200

    Add C# program to count words in a sentence
    Introduced a new C# program in `task6.cs` to count the number of words in a user-provided sentence. Added necessary `using` directives, created a new namespace `strings_and_arrays`, and defined an internal class `task6`. Implemented the `Main` method to prompt user input and call the `CountWords` method, which splits the sentence into words and returns the count.

---
+++ task6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_and_arrays
+{
+    internal class task6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            int wordCount = CountWords(sentence);
+            Console.WriteLine("Number of words: " + wordCount);
+        }
+
+        static int CountWords(string sentence)
+        {
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+}
