e4115c72c8fd171ec14492af55515b891595365f
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 14:13:00 +0200

    Add task4.cs to find and print common elements in arrays
    Introduce a new C# program in task4.cs with necessary using directives.
    Create a namespace `strings_and_arrays` and define an internal class `task4`.
    Add the `Main` method to define two integer arrays, use `Intersect` to find common elements, and print them without duplicates.

---
+++ task4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_and_arrays
+{
+    internal class task4
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 5, 6 };
+            int[] array2 = { 4, 5, 6, 7, 8, 8 };
+
+            int[] commonElements = array1.Intersect(array2).ToArray();
+
+            Console.WriteLine("Common elements without duplicates:");
+            foreach (int num in commonElements)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
