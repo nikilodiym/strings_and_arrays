cfb0612fbe61e86a197f78648d58597153e61b2c
nikilodiym <nikitabaram@gmail.com>
2024-11-16 17:12:01 +0200

    Add Caesar cipher Encrypt and Decrypt methods
    Introduce a new namespace `strings_and_arrays` with an internal class `task12`. This class includes two static methods: `Encrypt` and `Decrypt`. The `Encrypt` method applies a Caesar cipher to shift letters in the input string by a specified number of positions, while the `Decrypt` method reverses this shift to return the original string. Non-letter characters remain unchanged.

---
+++ task12.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_and_arrays
+{
+    internal class task12
+    {
+        public static string Encrypt(string input, int shift)
+        {
+            string encryptedText = "";
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char encryptedChar = (char)(((c - 'A') + shift) % 26 + 'A');
+                    encryptedText += encryptedChar;
+                }
+                else
+                {
+                    encryptedText += c;
+                }
+            }
+            return encryptedText;
+        }
+
+        public static string Decrypt(string input, int shift)
+        {
+            string decryptedText = "";
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char decryptedChar = (char)(((c - 'A') - shift + 26) % 26 + 'A');
+                    decryptedText += decryptedChar;
+                }
+                else
+                {
+                    decryptedText += c;
+                }
+            }
+            return decryptedText;
+        }
+    }
+}
