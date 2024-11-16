e5fa30ca8a94d53ff275a313fc447e40381bcb9e
nikilodiym <nikitabaram@gmail.com>
2024-11-16 17:11:18 +0200

    Add program to sum elements between min and max in 2D array
    Introduced a new C# program in the `strings_and_arrays` namespace,
    within the `task11` class. The program initializes a 5x5
    two-dimensional array with random integers ranging from -100 to 100.
    It finds the minimum and maximum values in the array, calculates the
    sum of all elements between their first occurrences, and prints the
    result to the console.

---
+++ task11.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_and_arrays
+{
+    internal class task11
+    {
+        public static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-100, 101);
+                }
+            }
+
+            int min = array[0, 0];
+            int max = array[0, 0];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+
+            int sum = 0;
+            bool foundMin = false;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (array[i, j] == min)
+                    {
+                        foundMin = true;
+                    }
+                    else if (array[i, j] == max)
+                    {
+                        foundMin = false;
+                    }
+                    else if (foundMin)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Sum of elements between min and max: " + sum);
+        }
+    }
+}
