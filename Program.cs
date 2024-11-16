59e3eb6a2ca0a3d81f93ec799106a8c3b96abf2c
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 13:16:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace strings_and_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write elements of the array separated by space: ");
+
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+            int ev = array.Count(x => x % 2 == 0);
+            int od = array.Count(x => x % 2 != 0);
+            int ui = array.Distinct().Count();
+
+            Console.WriteLine($"Even numbers: {ev}");
+            Console.WriteLine($"Odd numbers: {od}");
+            Console.WriteLine($"Unique numbers: {ui}");
+        }
+    }
+}
