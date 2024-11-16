60e4cf53feea4c2e1aed706aa562319cbcf89ff7
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 13:41:09 +0200

    Refactor: Remove user input and array processing logic
    The code that prompts the user for input, processes the input to
    create an array, and calculates and prints the counts of even,
    odd, and unique numbers has been removed from the Main method
    in Program.cs. This results in a Main method that no longer
    performs any operations or outputs any information to the console.

--- Program.cs
+++ Program.cs
@@ -6,17 +6,7 @@ namespace strings_and_arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Write elements of the array separated by space: ");
-
-            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             
-            int ev = array.Count(x => x % 2 == 0);
-            int od = array.Count(x => x % 2 != 0);
-            int ui = array.Distinct().Count();
-
-            Console.WriteLine($"Even numbers: {ev}");
-            Console.WriteLine($"Odd numbers: {od}");
-            Console.WriteLine($"Unique numbers: {ui}");
         }
     }
 }
